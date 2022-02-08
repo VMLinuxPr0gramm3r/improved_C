99b3e7ea11c091da6bd38644437c9a1855c689e9
VMLinuxPr0gramm3r <99178290+VMLinuxPr0gramm3r@users.noreply.github.com>
2022-02-08 11:29:34 +0530

    Create println.h

---
+++ headers/println.h
@@ -0,0 +1,13 @@
+/*
+author : Venkatesh Mishra
+compiler : gcc
+function : prinln
+file : system.h
+description : function for simple string output
+*/
+#include <stdio.h>
+void println(char * str){
+    for(int short i =0;i < 1; i++){
+        printf("%s\n",str);
+    }
+}
